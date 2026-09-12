1d9e16d38ccc7418c985a48b74dc869e6ed3b829
budwol <wolf.budgenhagen@gmail.com>
2026-09-12 13:10:50 +0200

    :zap: Serve smaller avatar assets on wide screens

--- src/constants/imageConstants.ts
+++ src/constants/imageConstants.ts
@@ -1,6 +1,6 @@
 export const imageConstants = {
   avatarResponsiveSize: 300,
-  avatarResponsiveViewportWidth: 1200,
+  avatarResponsiveViewportWidth: 2048,
   avatarOriginalSize: 1024,
   projectImageBreakpointSmall: 480,
   projectImageBreakpointMedium: 960,
